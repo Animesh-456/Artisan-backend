4bc7f5546ee3e49aadee25d254dd344c97701ee1
sweety-cool <sweety127ahmed@gmail.com>
2024-11-22 11:45:58 +0530

    change

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,17 +4,17 @@ import { Dialect } from "sequelize";
 dotenv.config();
 
 export default {
-    // MySql
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    db: 'art_data',
-    dialect: "mysql" as Dialect,
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 1000000,
-        idle: 10000,
-        port: 3306,
-    },
-}; 
+ // MySql
+ host: '188.245.62.232',
+ user: 'art',
+ password: 'artisian',
+ db: 'art',
+ dialect: "mysql" as Dialect,
+ pool: {
+  max: 5,
+  min: 0,
+  acquire: 1000000,
+  idle: 10000,
+  port: 3306,
+ },
+};
